bf8db8f4669ce707df2a3b592dc5b5b1e3c88de8
Masataka Miyoshi <masataka.ehime.univ@gmail.com>
2017-08-07 19:23:21 +0900

    Initial Commit

---
+++ ARKitTutorial/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  ARKitTutorial
+//
+//  Created by Miyoshi Masataka on 2017/08/06.
+//  Copyright © 2017年 Masataka Miyoshi. All rights reserved.
+//
+
+import UIKit
+import ARKit
+import SceneKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var sceneView: ARSCNView!
+    
+    // Player Settingsの"Camera Usage Description"に何か文字列を入れる
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let configration = ARWorldTrackingSessionConfiguration()
+        configration.planeDetection = .horizontal
+        
+        sceneView.session.run(configration)
+    }
+
+    // Cubeを追加する
+    @IBAction func addCube(_ sender: Any) {
+        let cubeNode = SCNNode(geometry: SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0))
+        cubeNode.position = SCNVector3(0, 0, 0)
+        sceneView.scene.rootNode.addChildNode(cubeNode)
+    }
+    
+    @IBAction func addCup(_ sender: Any) {
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
