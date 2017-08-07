f7cbc45eb466191ad3ca2457c46d76277448ace7
Masataka Miyoshi <masataka.ehime.univ@gmail.com>
2017-08-08 03:13:29 +0900

    Create random addBox

--- ARKitTutorial/ViewController.swift
+++ ARKitTutorial/ViewController.swift
@@ -25,20 +25,64 @@ class ViewController: UIViewController {
         
         sceneView.session.run(configration)
     }
+    
+    func randomFloat(min: Float, max: Float) -> Float {
+        return (Float(arc4random()) / 0xFFFFFFFF) * (max - min) + min
+    }
 
     // Cubeを追加する
     @IBAction func addCube(_ sender: Any) {
+        // ランダムでx,y,z軸の位置を決定
+//        let xCoords = randomFloat(min: -1, max: -0.2)
+//        let yCoords = randomFloat(min: -1, max: -0.2)
+//        let zCoords = randomFloat(min: -1, max: -0.2)
+        
+        // ノード作成
         let cubeNode = SCNNode()
+        // 正方形の形を定義
         let cubeGeometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0)
         
+        // ノードのgeometryに追加
         cubeNode.geometry = cubeGeometry
         
-        cubeNode.position = SCNVector3(0, 0, 0.5)
+//        cubeNode.position = SCNVector3(xCoords, yCoords, zCoords)
+        
         sceneView.scene.rootNode.addChildNode(cubeNode)
+        
+        //----------------------------------------------------
+        let cubeMaterial = SCNMaterial()
+        
+        // cubeMaterial.diffuse.contents = UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 1.0)
+        
+        // 画像の読み込み
+        cubeMaterial.diffuse.contents = UIImage(named: "popstar")
+        
+        cubeGeometry.firstMaterial = cubeMaterial
+        //----------------------------------------------------
     }
     
     @IBAction func addCup(_ sender: Any) {
     }
+    
+    struct myCameraCoordinates {
+        var x = Float()
+        var y = Float()
+        var z = Float()
+    }
+    
+    func getCameraCoordinates(sceneView: ARSCNView) -> myCameraCoordinates {
+        let cameraTransform = sceneView.session.currentFrame?.camera.transform
+        let cameraCoordinates = MDLTransform(matrix: cameraTransform!)
+        
+        var cc = myCameraCoordinates()
+        cc.x = cameraCoordinates.translation.x
+        cc.y = cameraCoordinates.translation.y
+        cc.z = cameraCoordinates.translation.z
+        
+        return cc
+    }
+    
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -47,4 +91,8 @@ class ViewController: UIViewController {
 
 
 }
+
+
+
+
 
